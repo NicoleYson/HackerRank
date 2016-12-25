f4ccb37e3e8ccedb7347ce9e0ddc8b495983eef6
Nicole Yson <nicoleyson@gmail.com>
2016-12-25 11:04:41 -0800

    Update SavePrincess.c

--- AI/SavePrincess.c
+++ AI/SavePrincess.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <math.h>
 void displayPathtoPrincess(int n, char grid[101][101]){
-    int row, col, y, x;
+    int row, col, x, y;
     short mario[2], princess[2]; //declaring short arrays of size 2
 
   for(row = 0; row < n; row++) { //traverse the 2d array by rows and cols
@@ -18,6 +18,14 @@ void displayPathtoPrincess(int n, char grid[101][101]){
     }
   }
 
+  if ((x = princess[1] - mario[1]) < 0) { //if delta of x coordinates are negative
+    for(; x < 0; x++) //while delta x is negative, increment x in order to move left
+      printf("LEFT\n");
+  } else {
+    for(; x > 0; x--) //while delta x is positive, decement x to move right
+      printf("RIGHT\n");
+  }
+
   if ((y = princess[0] - mario[0]) < 0) { //if delta of y coordinates are negative
     for(; y < 0; y++) //while delta y is negative, increment y in order to move up
       printf("UP\n");
@@ -26,14 +34,7 @@ void displayPathtoPrincess(int n, char grid[101][101]){
       printf("DOWN\n");
   }
 
-  if ((x = princess[1] - mario[1]) < 0) { //if delta of x coordinates are negative
-    for(; x < 0; x++) //while delta x is negative, increment x in order to move left
-      printf("LEFT\n");
-  } else {
-    for(; x > 0; x--) //while delta x is positive, decement x to move right
-      printf("RIGHT\n");
-  }
-}//the loop exits when the distance is 0
+}//the loop ends when the distance is 0
 
 int main(void) { 
 
