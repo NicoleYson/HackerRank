ac664a4fc2e099abb842784dd05f95b1aab16a07
Nicole Yson <nicoleyson@gmail.com>
2016-12-25 11:01:23 -0800

    Update SavePrincess.c

--- AI/SavePrincess.c
+++ AI/SavePrincess.c
@@ -12,28 +12,29 @@ void displayPathtoPrincess(int n, char grid[101][101]){
         mario[1] = col; //set y coordinate portion of mario array to current column 
       }
       if (grid[row][col] == 'p') { //if current index contains Princess Peach 'p'
-        princess[0] = row;
-        princess[1] = col;
+        princess[0] = row; //set x coordinates portion of peach array to equal current row
+        princess[1] = col; //set y coorinates of peach array to equal current row
       }
     }
   }
 
   if ((y = princess[0] - mario[0]) < 0) { //if delta of y coordinates are negative
-    for(; y < 0; y++)
+    for(; y < 0; y++) //while delta y is negative, increment y in order to move up
       printf("UP\n");
   } else {
-    for(; y > 0; y--)
+    for(; y > 0; y--) //while delta y is positive, decement y to move down
       printf("DOWN\n");
   }
 
   if ((x = princess[1] - mario[1]) < 0) { //if delta of x coordinates are negative
-    for(; x < 0; x++)
+    for(; x < 0; x++) //while delta x is negative, increment x in order to move left
       printf("LEFT\n");
   } else {
-    for(; x > 0; x--)
+    for(; x > 0; x--) //while delta x is positive, decement x to move right
       printf("RIGHT\n");
   }
-}
+}//the loop exits when the distance is 0
+
 int main(void) { 
 
     int m; //bot position
@@ -48,4 +49,3 @@ int main(void) {
     displayPathtoPrincess(m,grid);
     return 0;
 }
-
